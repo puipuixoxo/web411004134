10e1444053145208b2743badb85255b6a5de5297
wei <e09358399@gmail.com>
2023-05-31 18:28:53 +0800

    Change name to Username

--- Web411004134/Models1/Partials/Student.cs
+++ Web411004134/Models1/Partials/Student.cs
@@ -17,7 +17,7 @@ namespace Web411004134.Models1
         [Required(ErrorMessage = "請填寫姓名")]
         [Display(Name = "姓名")]
         [StringLength(5,MinimumLength =2,ErrorMessage ="請輸入2~5個字")]
-        public string Name{ get ; set ; }
+        public string Username{ get ; set ; }
 
         [Required(ErrorMessage = "請填寫學號")]
         [Display(Name = "學號")]
